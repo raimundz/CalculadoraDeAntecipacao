7ef025247a14329ba7f80f228e62acf03368dac4
= <r.netofreire45@gmail.com>
2023-01-19 14:28:53 -0300

    MVP finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { MainContainer } from "./components/container";
 import "./App.css";
-
+//validação de dados e tratativa de erros com yup
+//usar o timeout pra alguns possiveiss problemas com a api
 function App() {
   const [count, setCount] = useState(0);
 
